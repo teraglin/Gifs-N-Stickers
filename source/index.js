665bd3be92d7d00cd02ff88adffae4026cb8922c
teraglin <apkirvan@hotmail.com>
2021-05-06 12:45:12 +1000

    added test for api key and initial api logic to index.js

--- source/index.js
+++ source/index.js
@@ -1,13 +1,47 @@
 import { apiKey } from './keys.js'
 
-const baseUrl = "api.giphy.com/v1/"
+const baseUrl = "https://api.giphy.com/v1/"
 const gifSearch = "gifs/search"
 const stickerSearch = "stickers/search"
 
-function getGif(query) {
 
+
+async function getGif(query) {
+
+    let data = null
+
+    try {
+        let response = await fetch(`${baseUrl}${gifSearch}?q=${query}&${apiKey}`)
+        data = await response.json()
+        console.log(data)
+    }
+
+    catch (error) {
+        console.log(error.message)
+    }
+
+    finally {
+        console.log("hello gif")
+    }
 }
 
 function getSticker(query) {
-  
+    let data = null
+
+    try {
+        let response = await fetch(`${baseUrl}${stickerSearch}?q=${query}&${apiKey}`)
+        data = await response.json()
+        console.log(data)
+    }
+
+    catch (error) {
+        console.log(error.message)
+    }
+
+    finally {
+        console.log("hello sticker")
+    }
 }
+
+getGif("cheeseburger")
+getSticker("cheeseburger")
