609cc9907715736fc3959469816ac62119e60260
victorzottmann <zottmann.victor@gmail.com>
2021-05-06 23:38:28 +1000

    clean up code repetition and improve variable names

--- source/index.js
+++ source/index.js
@@ -1,23 +1,24 @@
 import { apiKey } from './keys.js'
 
 const baseUrl = "https://api.giphy.com/v1/"
-const gifSearch = "gifs/search"
-const stickerSearch = "stickers/search"
-
 const searchInput = document.getElementById("search-input")
 const searchButton = document.getElementById("search-btn")
 const gifsContainer = document.querySelector(".gifs-container")
+
 
 //--------- ACTIVE BUTTONS
 
 // Get container element for nav buttons
 const navButtons = document.getElementById('nav-buttons')
 
+
 // Get all buttons with class="btn" inside the container
 const btns = navButtons.getElementsByClassName('btn')
 
+
 let current = null
 let currentText = null
+
 
 // loop through all the buttons and add the active class to the current/clicked button
 for (let i=0; i < btns.length; i++) {
@@ -30,63 +31,21 @@ for (let i=0; i < btns.length; i++) {
     this.className += " active"
     currentText = current[0].textContent
     
-    if (currentText === 'GIF') {
-      searchButton.textContent = "Search GIFs"
-    } else {
-      searchButton.textContent = "Search Stickers"
-    }
+    changeSearchButtonText()
   })
 }
 
 
 //-----------
 
-async function getGif() {
-  clearExisting()
-
-  const inputText = searchInput.value
-  let data = null
-
-  try {
-    let response = await fetch(`${baseUrl}${gifSearch}?q=${inputText}&${apiKey}`)
-    data = await response.json()
-
-    for (let i = 0; i < 10; i++) {
-      createNewGif(data.data[i].images.original.url)
-    }
-  } catch (error) {
-    console.log(error.message)
+function changeSearchButtonText() {
+  if (currentText === 'GIF') {
+    searchButton.textContent = "Search GIFs"
+  } else {
+    searchButton.textContent = "Search Stickers"
   }
 }
 
-async function getSticker() {
-  clearExisting()
-
-  const inputText = searchInput.value
-  let data = null
-
-  try {
-    let response = await fetch(`${baseUrl}${stickerSearch}?q=${inputText}&${apiKey}`)
-    data = await response.json()
-
-    for (let i = 0; i < 10; i++) {
-      createNewGif(data.data[i].images.original.url)
-    }
-
-  } catch (error) {
-    console.log(error.message)
-  }
-}
-
-function clearExisting(params) {
-  const gifElement = document.getElementsByClassName("gif-element")
-
-  for (let i = 0; i < gifsContainer.children.length; i++) {
-    if (gifsContainer.children.length > 0) {
-      gifsContainer.removeChild(gifElement[i])
-    }
-  }
-}
 
 function createNewGif(url) {
   const gifElement = document.createElement("img")
@@ -95,10 +54,45 @@ function createNewGif(url) {
   gifsContainer.appendChild(gifElement)
 }
 
+
+function clearExisting() {
+  const gifElement = document.getElementsByClassName("gif-element")
+
+  // loop through the gifs container div and check how many children (<img>) are present
+  for (let i = 0; i < gifsContainer.children.length; i++) {
+    if (gifsContainer.children.length > 0) {
+      gifsContainer.removeChild(gifElement[i])
+    }
+  }
+}
+
+
+async function search(searchEndpoint) {
+  clearExisting()
+
+  const inputText = searchInput.value
+  let jsonData = null
+
+  try {
+    let response = await fetch(`${baseUrl}${searchEndpoint}?q=${inputText}&${apiKey}`)
+    jsonData = await response.json()
+
+    for (let i = 0; i < 10; i++) {
+      createNewGif(jsonData.data[i].images.original.url)
+    }
+
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+
 searchButton.addEventListener('click', () => {
   if (currentText === 'GIF') {
-    getGif()
+    const gifSearch = "gifs/search" 
+    search(gifSearch)
   } else if (currentText === 'STICKER') {
-    getSticker()
+    const stickerSearch = "stickers/search"
+    search(stickerSearch)
   }
 })
