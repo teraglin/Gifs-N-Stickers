80242ec6f329510d5a653b46ec29b493750dc986
victorzottmann <zottmann.victor@gmail.com>
2021-05-06 10:55:23 +1000

    setup giphy api

---
+++ source/index.js
@@ -0,0 +1,13 @@
+import { apiKey } from './keys.js'
+
+const baseUrl = "api.giphy.com/v1/"
+const gifSearch = "gifs/search"
+const stickerSearch = "stickers/search"
+
+function getGif(query) {
+
+}
+
+function getSticker(query) {
+  
+}
